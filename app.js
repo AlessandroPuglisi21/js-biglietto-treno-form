84cc093b4a0064fe758b3de1e8e12beda27db985
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-23 17:14:23 +0200

    ADDED:Js Ticket Generation

--- app.js
+++ app.js
@@ -1,11 +1,73 @@
-console.log('test')
+//!NOME DEL PASSEGGERO
 
-//NOME DEL PASSEGGERO
-document.getElementById('btnGeneration').addEventListener('click', function() {
-    // Recupera il valore del campo nome
-    const nomeInserito = document.getElementById('nome').value;
-  
-    // Aggiorna la tabella con il nome del passeggero
-    document.getElementById('nomePasseggero').textContent = nomeInserito;
-  });
-  
+//Qui Avvio l'Evento e la stampa con il click sul Bottone
+document.getElementById("btnGeneretion").addEventListener("click", function () {
+  //Da qui ottengo l'input del nome che andró a Stampare
+  let nome = document.getElementById("nome").value;
+
+  //Da qui parte la stampa
+  document.getElementById("nomePasseggeroStamp").innerText = nome;
+  //! CARROZZA
+  //Creo un let che generi un numero casuale da 1 a 10 quando viene cliccato il bottone
+  let carrozza = Math.floor(Math.random() * 10) + 1;
+
+  //Stampa del numero generato
+  document.getElementById("carrozzaStamp").innerText = carrozza;
+
+  //!CODICE CP
+  //Creo un let che generi un numero casuale da 1 a 10000 quando viene cliccato il bottone
+
+  let cpNumber = Math.floor(Math.random() * 10000) + 1;
+
+  //Stampa del numero generato
+  document.getElementById("cpStamp").innerText = cpNumber;
+
+  //! GENERAZIONE OFFERTA
+  let eta = document.getElementById('eta').value.toLowerCase(); 
+  let offerta = ''
+
+  if (eta.includes('minorenne')){
+    offerta = 'Sconto del 20%';
+  } else if(eta.includes('over 65')){
+    offerta = 'Sconto del 30%';
+  }else {
+    offerta = 'Biglietto Standard';
+  }
+  document.getElementById('offertaStamp').innerText = offerta;
+
+  //! COSTO DEL BIGLIETTO 
+  let km = document.getElementById('km').value
+  const costoBase = km *0.21
+  let price = ''
+
+  if (eta.includes('minorenne')){
+    price = costoBase * 0.80
+   } else if(eta.includes('over 65')){
+    price = costoBase * 0.70
+   } else {
+    price = costoBase
+   }
+
+   document.getElementById('totalPrice').innerText = `€ ${price}`;
+});
+
+//SCELTA CASUALE DELLA CARROZZA
+
+// document.getElementById('btnGeneretion').addEventListener('click', function(){
+
+//     //Creo un let che generi un numero casuale da 1 a 10 quando viene cliccato il bottone
+//     let carrozza = Math.floor(Math.random() * 10) + 1;
+
+//     //Stampa del numero generato
+//     document.getElementById('carrozzaStamp').innerText = carrozza;
+// })
+
+// //CODICE CP
+// document.getElementById("btnGeneretion").addEventListener("click", function () {
+//   //Creo un let che generi un numero casuale da 1 a 10000 quando viene cliccato il bottone
+
+//   let cpNumber = Math.floor(Math.random() * 10000) + 1;
+
+//   //Stampa del numero generato
+//   document.getElementById("cpStamp").innerText = cpNumber;
+// });
