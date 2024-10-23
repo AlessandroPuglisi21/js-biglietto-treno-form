69cf2f388379018587a39c4b910f5be2e7f1616a
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-23 16:27:58 +0200

    ADDED:Update Html

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('test')
+
+//NOME DEL PASSEGGERO
+document.getElementById('btnGeneration').addEventListener('click', function() {
+    // Recupera il valore del campo nome
+    const nomeInserito = document.getElementById('nome').value;
+  
+    // Aggiorna la tabella con il nome del passeggero
+    document.getElementById('nomePasseggero').textContent = nomeInserito;
+  });
+  
